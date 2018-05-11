a45ad126bcbd3320be3b0e414ba8211944626335
Md Omar Faroque <faroque.omar@outlook.com>
2018-05-11 16:29:34 -0400

    Initial Commit

---
+++ src/BasicDatatypes.java
@@ -0,0 +1,22 @@
+
+public class BasicDatatypes {
+
+	public static void main(String[] args) {
+		System.out.println("Working... Hello Sajib");
+		int a = 30;
+		int b = 20;
+		System.out.println(a);
+		System.out.println(b);
+		
+		double c = 2.90;
+		float d = 3.8f;
+		
+		int e = a + b;
+		System.out.println(e);
+		
+		int gg = a + c;
+		
+
+	}
+
+}
